00c17bfa5a49391c81c91cff08bc94bb5e4bd104
Keu <keuxd2003@gmail.com>
2024-10-07 05:54:02 -0300

    Base website for CI testing commited

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Moji-Matcher
+# Moji Matcher
+
 Moji Matcher é um aplicativo de namoro único projetado especificamente para indivíduos que são analfabetos ou preferem se comunicar por meio de meios visuais. O aplicativo se concentra no uso de emojis como linguagem principal, permitindo que os usuários expressem-se e se conectem com outros de forma divertida e intuitiva.
---
+++ src/Moji.vue
@@ -0,0 +1,46 @@
+<script>
+    export default {
+        data() {
+            return {
+                datas:"HABEMOS UNO BOTON",
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <header>
+        <div>
+            <a href="/">
+                <img src="./assets/logo-trans.png" id="logo">
+            </a>
+        </div>
+        <a href="#section1" class="section">Sobre</a>
+        <a href="#section2" class="section">Contato</a>
+        <a href="#section3" class="section">Baixar</a>
+        <button id="header-button">Experimente o Moji Matcher</button>
+
+    </header>
+
+    <div>
+        <img src="./assets/libs.png" id="home-libs">
+        
+    </div>
+
+    <section id="section1">
+        <h1>Sobre</h1>
+        <p>Sobre o aplicativo...</p>
+    </section>
+
+    <section id="section2">
+        <h1>Contato</h1>
+        <p>Descrição de contato</p>
+    </section>
+
+    <section id="section3">
+        <h1>Baixar</h1>
+        <p>Descrição para baixar</p>
+    </section>
+
+</template>
