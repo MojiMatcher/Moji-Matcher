bbbfd02c21306e8d8777eebb2345f3e4223f9a61
Keu <keuxd2003@gmail.com>
2024-10-07 08:26:07 -0300

    css fixes

--- src/Moji.vue
+++ src/Moji.vue
@@ -17,28 +17,28 @@
             </a>
         </div>
         <a href="#section1" class="section">Sobre</a>
-        <a href="#section2" class="section">Contato</a>
+        <a href="#section2" class="section">Funcionamento</a>
         <a href="#section3" class="section">Baixar</a>
-        <button id="header-button">Experimente o Moji Matcher</button>
+        <button id="header-button" onclick="window.location='www.google.com.br';">Experimente o Moji Matcher</button>
 
     </header>
 
-    <div>
+    <div id="home">
         <img src="./assets/libs.png" id="home-libs">
-        
+        <img src="./assets/sample.png" id="home-sample">
     </div>
 
-    <section id="section1">
+    <section id="section1" class="sectionTitle">
         <h1>Sobre</h1>
-        <p>Sobre o aplicativo...</p>
+        <p>Moji Matcher é um aplicativo para conhecer pessoas único projetado especificamente para indivíduos que são analfabetos ou preferem se comunicar por meio de meios visuais.<br><br><br>O aplicativo se concentra no uso de emojis como linguagem principal, permitindo que os usuários expressem-se e se conectem com outros de forma divertida e intuitiva.</p>
     </section>
 
-    <section id="section2">
-        <h1>Contato</h1>
-        <p>Descrição de contato</p>
+    <section id="section2" class="sectionTitle">
+        <h1>Funcionalidades</h1>
+        <p></p>
     </section>
 
-    <section id="section3">
+    <section id="section3" class="sectionTitle">
         <h1>Baixar</h1>
         <p>Descrição para baixar</p>
     </section>
